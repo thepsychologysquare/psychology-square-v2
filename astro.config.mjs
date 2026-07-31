2fd526762535bf739671c1a83508823a264831c8
Sohail <you@example.com>
2026-07-31 14:40:39 +0500

    astro.config fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+  adapter: cloudflare({
+    prerenderEnvironment: 'node',
+  }),
 });
